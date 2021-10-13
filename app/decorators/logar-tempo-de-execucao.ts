cbfb8a0375f5cb75cb9addc2548591391100c4d0
cassiomatoso <cassiocg98@gmail.com>
2021-10-13 20:36:04 -0300

    Finalizando Decorator

--- app/decorators/logar-tempo-de-execucao.ts
+++ app/decorators/logar-tempo-de-execucao.ts
@@ -1,7 +1,22 @@
 export function logarTempoDeExecucao() {
 
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+
+        const metodoOriginal = descriptor.value;
+
+        descriptor.value = function (...args: Array<any>) {
+            const tempo1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const tempo2 = performance.now();
+            let divisor = 1000;
+
+            console.log(`Método: ${propertyKey} - tempo de execução: ${(tempo2 - tempo1) / divisor} segundos`);
+
+            retorno;
+        };
+
         return descriptor;
+
     }
 
 }
