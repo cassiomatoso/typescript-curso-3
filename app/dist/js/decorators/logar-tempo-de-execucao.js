5406b828ac79f6b580be5cdbf4ce63cbcae70783
cassiomatoso <cassiocg98@gmail.com>
2021-10-13 20:58:01 -0300

    Corrigindo diretórios

--- app/dist/js/decorators/logar-tempo-de-execucao.js
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -2,11 +2,10 @@ export function logarTempoDeExecucao() {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
-            const tempo1 = performance.now();
+            const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
-            const tempo2 = performance.now();
-            let divisor = 1000;
-            console.log(`Método: ${propertyKey} - tempo de execução: ${(tempo2 - tempo1) / divisor} segundos`);
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`);
             retorno;
         };
         return descriptor;
