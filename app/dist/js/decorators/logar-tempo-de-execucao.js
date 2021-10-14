98012744ee05427f1b34d54af5d40fdd1754a21e
cassiomatoso <cassiocg98@gmail.com>
2021-10-13 21:18:49 -0300

    js gerados

--- app/dist/js/decorators/logar-tempo-de-execucao.js
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -2,10 +2,11 @@ export function logarTempoDeExecucao() {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
-            const t1 = performance.now();
+            const tempo1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
-            const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`);
+            const tempo2 = performance.now();
+            let divisor = 1000;
+            console.log(`Método: ${propertyKey} - tempo de execução: ${(tempo2 - tempo1) / divisor} segundos`);
             retorno;
         };
         return descriptor;
